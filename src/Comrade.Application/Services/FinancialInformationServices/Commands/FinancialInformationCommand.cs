ab311a5ec54ab5e625d088efd2139efbb1bd4c03
edsonrdl <edsonrodrigo11111@gmail.com>
2022-06-13 15:48:39 -0300

    feat: Adicionado FinancialInformation a Commands

---
+++ src/Comrade.Application/Services/FinancialInformationServices/Commands/FinancialInformationCommand.cs
@@ -0,0 +1,36 @@
+﻿using Comrade.Application.Bases;
+using Comrade.Application.Bases.Interfaces;
+using Comrade.Application.Services.FinancialInformationServices.Dtos;
+using Comrade.Core.FinancialInformationCore;
+using MediatR;
+
+namespace Comrade.Application.Services.FinancialInformationServices.Commands;
+
+public class FinancialInformationCommand : IFinancialInformationCommand
+{
+    private readonly IUcFinancialInformationDelete _deleteFinancialInformation;
+    private readonly IMediator _mediator;
+
+    public FinancialInformationCommand(
+        IUcFinancialInformationDelete deleteFinancialInformation, IMediator mediator)
+    {
+        _deleteFinancialInformation = deleteFinancialInformation;
+        _mediator = mediator;
+    }
+
+    public async Task<ISingleResultDto<EntityDto>> Create(FinancialInformationCreateDto dto)
+    {
+        return await _mediator.Send(dto).ConfigureAwait(false);
+    }
+
+    public async Task<ISingleResultDto<EntityDto>> Edit(FinancialInformationEditDto dto)
+    {
+        return await _mediator.Send(dto).ConfigureAwait(false);
+    }
+
+    public async Task<ISingleResultDto<EntityDto>> Delete(Guid id)
+    {
+        var result = await _deleteFinancialInformation.Execute(id).ConfigureAwait(false);
+        return new SingleResultDto<EntityDto>(result);
+    }
+}
---
+++ src/Comrade.Application/Services/FinancialInformationServices/Commands/IFinancialInformationCommand.cs
@@ -0,0 +1,12 @@
+﻿using Comrade.Application.Bases;
+using Comrade.Application.Bases.Interfaces;
+using Comrade.Application.Services.FinancialInformationServices.Dtos;
+
+namespace Comrade.Application.Services.FinancialInformationServices.Commands;
+
+public interface IFinancialInformationCommand
+{
+    Task<ISingleResultDto<EntityDto>> Create(FinancialInformationCreateDto dto);
+    Task<ISingleResultDto<EntityDto>> Edit(FinancialInformationEditDto dto);
+    Task<ISingleResultDto<EntityDto>> Delete(Guid id);
+}
