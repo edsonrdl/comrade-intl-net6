a845313f7bbcd37a117ee728801510a5ace70dc1
edsonrdl <edsonrodrigo11111@gmail.com>
2022-06-13 15:53:26 -0300

    feat: Criando Handlers da entidade FinancialInformation

---
+++ src/Comrade.Application/Services/FinancialInformationServices/Handlers/FinancialInformationCreateHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Comrade.Application.Bases;
+using Comrade.Application.Services.FinancialInformationServices.Dtos;
+using Comrade.Core.FinancialInformationCore;
+using Comrade.Core.FinancialInformationCore.Commands;
+using MediatR;
+
+namespace Comrade.Application.Services.FinancialInformationServices.Handlers;
+
+public class
+    FinancialInformationCreateHandler : IRequestHandler<FinancialInformationCreateDto, SingleResultDto<EntityDto>>
+{
+    private readonly IUcFinancialInformationCreate _createFinancialInformation;
+    private readonly IMapper _mapper;
+
+    public FinancialInformationCreateHandler(IMapper mapper, IUcFinancialInformationCreate createFinancialInformation)
+    {
+        _mapper = mapper;
+        _createFinancialInformation = createFinancialInformation;
+    }
+
+    public async Task<SingleResultDto<EntityDto>> Handle(FinancialInformationCreateDto request,
+        CancellationToken cancellationToken)
+    {
+        var mappedObject = _mapper.Map<FinancialInformationCreateCommand>(request);
+        var result = await _createFinancialInformation.Execute(mappedObject).ConfigureAwait(false);
+        return new SingleResultDto<EntityDto>(result);
+    }
+}
---
+++ src/Comrade.Application/Services/FinancialInformationServices/Handlers/FinancialInformationEditHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Comrade.Application.Bases;
+using Comrade.Application.Services.FinancialInformationServices.Dtos;
+using Comrade.Core.FinancialInformationCore;
+using Comrade.Core.FinancialInformationCore.Commands;
+using MediatR;
+
+namespace Comrade.Application.Services.FinancialInformationServices.Handlers;
+
+public class FinancialInformationEditHandler : IRequestHandler<FinancialInformationEditDto, SingleResultDto<EntityDto>>
+{
+    private readonly IUcFinancialInformationEdit _editFinancialInformation;
+    private readonly IMapper _mapper;
+
+    public FinancialInformationEditHandler(IMapper mapper, IUcFinancialInformationEdit editFinancialInformation)
+    {
+        _mapper = mapper;
+        _editFinancialInformation = editFinancialInformation;
+    }
+
+    public async Task<SingleResultDto<EntityDto>> Handle(FinancialInformationEditDto request,
+        CancellationToken cancellationToken)
+    {
+        var mappedObject = _mapper.Map<FinancialInformationEditCommand>(request);
+        var result = await _editFinancialInformation.Execute(mappedObject).ConfigureAwait(false);
+        return new SingleResultDto<EntityDto>(result);
+    }
+}
