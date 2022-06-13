618f3b9325bf89858394a9210040a2dbed609c9a
edsonrdl <edsonrodrigo11111@gmail.com>
2022-06-13 15:54:56 -0300

    feat: Criado queries da entidade FinancialInformation

---
+++ src/Comrade.Application/Services/FinancialInformationServices/Queries/FinancialInformationQuery.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Comrade.Application.Bases;
+using Comrade.Application.Bases.Interfaces;
+using Comrade.Application.Lookups;
+using Comrade.Application.Paginations;
+using Comrade.Application.Services.FinancialInformationServices.Dtos;
+using Comrade.Core.FinancialInformationCore;
+using Comrade.Domain.Models;
+
+namespace Comrade.Application.Services.FinancialInformationServices.Queries;
+
+public class FinancialInformationQuery : IFinancialInformationQuery
+{
+    private readonly IMapper _mapper;
+    private readonly IMongoDbQueryContext _mongoDbQueryContext;
+    private readonly IFinancialInformationRepository _repository;
+
+    public FinancialInformationQuery(IFinancialInformationRepository repository,
+        IMongoDbQueryContext mongoDbQueryContext, IMapper mapper)
+    {
+        _repository = repository;
+        _mongoDbQueryContext = mongoDbQueryContext;
+        _mapper = mapper;
+    }
+
+    public async Task<IPageResultDto<FinancialInformationDto>> GetAll(
+        PaginationQuery? paginationQuery = null)
+    {
+        var paginationFilter = _mapper.Map<PaginationQuery?, PaginationFilter?>(paginationQuery);
+
+        List<FinancialInformationDto> list;
+        if (paginationFilter == null)
+        {
+            list = await Task.Run(() => _repository.GetAllAsNoTracking()
+                .ProjectTo<FinancialInformationDto>(_mapper.ConfigurationProvider)
+                .ToList()).ConfigureAwait(false);
+
+            return new PageResultDto<FinancialInformationDto>(list);
+        }
+
+        var skip = (paginationFilter.PageNumber - 1) * paginationFilter.PageSize;
+
+        list = await Task.Run(() => _repository.GetAllAsNoTracking().Skip(skip)
+            .Take(paginationFilter.PageSize)
+            .ProjectTo<FinancialInformationDto>(_mapper.ConfigurationProvider)
+            .ToList()).ConfigureAwait(false);
+
+        return new PageResultDto<FinancialInformationDto>(paginationFilter, list);
+    }
+
+    public async Task<ListResultDto<LookupDto>> FindByName(string name)
+    {
+        var list = await Task.Run(() => _repository.FindByName(name)
+            .ProjectTo<LookupDto>(_mapper.ConfigurationProvider)
+            .ToList()).ConfigureAwait(false);
+
+        return new ListResultDto<LookupDto>(list);
+    }
+
+    public async Task<ISingleResultDto<FinancialInformationDto>> GetByIdDefault(Guid id)
+    {
+        var entity = await _repository.GetById(id).ConfigureAwait(false);
+        var dto = _mapper.Map<FinancialInformationDto>(entity);
+        return new SingleResultDto<FinancialInformationDto>(dto);
+    }
+
+    public async Task<ISingleResultDto<FinancialInformationDto>> GetByIdMongo(Guid id)
+    {
+        var entity = await _mongoDbQueryContext.GetById<FinancialInformation?>(id).ConfigureAwait(false);
+        var dto = _mapper.Map<FinancialInformationDto>(entity);
+        return new SingleResultDto<FinancialInformationDto>(dto);
+    }
+}
---
+++ src/Comrade.Application/Services/FinancialInformationServices/Queries/IFinancialInformationQuery.cs
@@ -0,0 +1,15 @@
+﻿using Comrade.Application.Bases;
+using Comrade.Application.Bases.Interfaces;
+using Comrade.Application.Lookups;
+using Comrade.Application.Paginations;
+using Comrade.Application.Services.FinancialInformationServices.Dtos;
+
+namespace Comrade.Application.Services.FinancialInformationServices.Queries;
+
+public interface IFinancialInformationQuery
+{
+    Task<IPageResultDto<FinancialInformationDto>> GetAll(PaginationQuery? paginationQuery = null);
+    Task<ISingleResultDto<FinancialInformationDto>> GetByIdDefault(Guid id);
+    Task<ISingleResultDto<FinancialInformationDto>> GetByIdMongo(Guid id);
+    Task<ListResultDto<LookupDto>> FindByName(string name);
+}
