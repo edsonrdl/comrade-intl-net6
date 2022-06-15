92faf3cb5df73f974940ce0276718642e55abdfb
edsonrdl <edsonrodrigo11111@gmail.com>
2022-06-15 14:57:42 -0300

    feat: Adicionando propriedades na models da FinancialInformation

--- src/Comrade.Domain/Models/FinacialInformation.cs
+++ src/Comrade.Domain/Models/FinacialInformation.cs
@@ -2,46 +2,54 @@ using Comrade.Domain.Bases;
 
 namespace Comrade.Domain.Models;
 
-[Table("syus_financialInformation_information")]
+[Table("fiin_financial_information")]
 public class FinancialInformation : Entity
 {
-    public FinancialInformation()
-    {
-        Name = "";
-        Password = "";
-        Registration = "";
-    }
+    public FinancialInformation(){}
 
-    public FinancialInformation(Guid id, string name, string email, string password, string registration,
-        DateTime? registerDate)
+    public FinancialInformation(Guid id, string type, string date, string
+value, string cPF, string card,string hour,string shop, string store)
     {
         Id = id;
-        Name = name;
-        Email = email;
-        Password = password;
-        Registration = registration;
-        RegisterDate = registerDate;
+        Type = type;
+        Date = date;
+        Value = value;
+        CPF = cPF;
+        Card = card;
+        Hour = hour;
+        Shop = shop;
+        Store = store;
     }
 
-    [Column("syus_tx_name", TypeName = "varchar")]
-    [MaxLength(255)]
-    [Required(ErrorMessage = "NOME USU is required")]
-    public string Name { get; set; } // varchar(255), not null
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(1)]
+    public string Type { get; set; }
 
-    [Column("syus_tx_email", TypeName = "varchar")]
-    [MaxLength(255)]
-    public string? Email { get; set; } // varchar(255), null
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(8)]
+    public string Date { get; set; }
 
-    [Column("syus_pw_password", TypeName = "varchar")]
-    [MaxLength(1023)]
-    [Required(ErrorMessage = "Password is required")]
-    public string Password { get; set; } // varchar(1023), not null
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(10)]
+    public string Value { get; set; }
 
-    [Column("syus_tx_registration", TypeName = "varchar")]
-    [MaxLength(255)]
-    [Required(ErrorMessage = "Registration is required")]
-    public string Registration { get; set; } // varchar(255), not null
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(11)]
+    public string CPF { get; set; }
 
-    [Column("syus_dt_register", TypeName = "varchar")]
-    public DateTime? RegisterDate { get; set; }
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(12)]
+    public string Card { get; set; }
+
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(6)]
+    public string Hour { get; set; }
+
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(6)]
+    public string Shop { get; set; }
+
+    [Column("fiin_tx_type", TypeName = "varchar")]
+    [MaxLength(6)]
+    public string Store { get; set; }
 }
