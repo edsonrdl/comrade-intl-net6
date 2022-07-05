41a47f7a1f563e357b201cfb90b6dd3f2b24d3dc
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-05 17:55:25 -0300

    feat: Cradio ManyResult do FinancialInformation

---
+++ src/Comrade.Core/Bases/Results/ManyResult .cs
@@ -0,0 +1,24 @@
+﻿using Comrade.Core.Messages;
+using Comrade.Domain.Bases;
+using Comrade.Domain.Enums;
+
+namespace Comrade.Core.Bases.Results
+{
+    public class ManyResult<TEntity> : SingleResult<TEntity>
+        where TEntity : Entity
+    {
+        public ManyResult()
+        {
+            Code = (int)EnumResponse.Manyd;
+            Success = true;
+            Message = BusinessMessage.MSG01;
+        }
+
+        public ManyResult(bool success, string? message)
+        {
+            Code = success ? (int)EnumResponse.Manyd : (int)EnumResponse.NotFound;
+            Success = success;
+            Message = message;
+        }
+    }
+}
