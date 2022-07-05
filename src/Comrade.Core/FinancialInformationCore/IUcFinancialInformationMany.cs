2a1bc85f403007f8ed6d044424e5aa005481c822
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-05 17:57:24 -0300

    feat: Criado IUcFinancialInformationMany

---
+++ src/Comrade.Core/FinancialInformationCore/IUcFinancialInformationMany.cs
@@ -0,0 +1,10 @@
+﻿using Comrade.Core.Bases.Interfaces;
+using Comrade.Core.FinancialInformationCore.Commands;
+using Comrade.Domain.Bases;
+
+namespace Comrade.Core.FinancialInformationCore;
+
+public interface IUcFinancialInformationCreateMany
+{
+    Task<ISingleResult<Entity>> Execute(FinancialInformationCreateManyCommand entity);
+}
