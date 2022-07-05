e3ffbf573d5645cd8b96c1398892356aa6039e74
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-05 17:56:01 -0300

    feat: Criado FinancialInformationManyCommand

---
+++ src/Comrade.Core/FinancialInformationCore/Commands/FinancialInformationManyCommand.cs
@@ -0,0 +1,12 @@
+﻿using Comrade.Core.Bases.Interfaces;
+using Comrade.Domain.Bases;
+using Comrade.Domain.Models;
+using MediatR;
+
+
+namespace Comrade.Core.FinancialInformationCore.Commands;
+
+public class FinancialInformationCreateManyCommand : IRequest<ISingleResult<Entity>>
+{
+    public IList<FinancialInformation> FinancialInformations { get; set; }
+}
