c07be44af7f37393c473d10a8ea4ebdbcecf9f46
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-05 17:37:13 -0300

    feat: Alterado propiedade cpf da FinanciamInformation

--- src/Comrade.Domain/Models/FinacialInformation.cs
+++ src/Comrade.Domain/Models/FinacialInformation.cs
@@ -8,13 +8,13 @@ public class FinancialInformation : Entity
     public FinancialInformation(){}
 
     public FinancialInformation(Guid id, string type, string date, string
-value, string cPF, string card,string hour,string shop, string store)
+value, string cpf, string card,string hour,string shop, string store)
     {
         Id = id;
         Type = type;
         Date = date;
         Value = value;
-        CPF = cPF;
+        Cpf = cpf;
         Card = card;
         Hour = hour;
         Shop = shop;
@@ -25,31 +25,31 @@ value, string cPF, string card,string hour,string shop, string store)
     [MaxLength(1)]
     public string Type { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
+    [Column("fiin_tx_date", TypeName = "varchar")]
     [MaxLength(8)]
     public string Date { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
+    [Column("fiin_tx_value", TypeName = "varchar")]
     [MaxLength(10)]
     public string Value { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
+    [Column("fiin_tx_cpf", TypeName = "varchar")]
     [MaxLength(11)]
-    public string CPF { get; set; }
+    public string Cpf { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
+    [Column("fiin_tx_card", TypeName = "varchar")]
     [MaxLength(12)]
     public string Card { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
+    [Column("fiin_tx_hour", TypeName = "varchar")]
     [MaxLength(6)]
     public string Hour { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
-    [MaxLength(6)]
+    [Column("fiin_tx_shop", TypeName = "varchar")]
+    [MaxLength(14)]
     public string Shop { get; set; }
 
-    [Column("fiin_tx_type", TypeName = "varchar")]
-    [MaxLength(6)]
+    [Column("fiin_tx_store", TypeName = "varchar")]
+    [MaxLength(19)]
     public string Store { get; set; }
 }
