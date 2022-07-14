a5ff06ddaf100201d3cb0de5c22a36075f113dfa
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-14 10:16:23 -0300

    feat: Type das propriedades FinancialInformation

--- src/Comrade.Application/Services/FinancialInformationServices/Dtos/FinancialInformationDto.cs
+++ src/Comrade.Application/Services/FinancialInformationServices/Dtos/FinancialInformationDto.cs
@@ -1,15 +1,16 @@
 ﻿using Comrade.Application.Bases;
+using Comrade.Domain.Enums;
 
 namespace Comrade.Application.Services.FinancialInformationServices.Dtos;
 
 public class FinancialInformationDto : EntityDto
 {
-    public string? Type { get; set; }
-    public string? Date { get; set; }
-    public string? Value { get; set; }
+    public EnumTypeFinancial? Type { get; set; }
+    public DateTime Date { get; }
+    public decimal? Value { get; set; }
     public string? Cpf { get; set; }
     public string? Card { get; set; }
-    public string? Hour { get; set; }
+    public DateTime? Hour { get; set; }
     public string? Shop { get; set; }
     public string? Store { get; set; }
    
