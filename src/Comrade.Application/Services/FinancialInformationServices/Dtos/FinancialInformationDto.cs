80303bc6d45a0ffa242536e89a485bef539dba87
edsonrdl <edsonrodrigo11111@gmail.com>
2022-07-25 11:41:56 -0300

    feat: Criado Typagem propriedade da FinancialInformationDto

--- src/Comrade.Application/Services/FinancialInformationServices/Dtos/FinancialInformationDto.cs
+++ src/Comrade.Application/Services/FinancialInformationServices/Dtos/FinancialInformationDto.cs
@@ -6,11 +6,10 @@ namespace Comrade.Application.Services.FinancialInformationServices.Dtos;
 public class FinancialInformationDto : EntityDto
 {
     public EnumTypeFinancial? Type { get; set; }
-    public DateTime Date { get; }
+    public DateTime? DateTime { get; set; }
     public decimal? Value { get; set; }
     public string? Cpf { get; set; }
     public string? Card { get; set; }
-    public DateTime? Hour { get; set; }
     public string? Shop { get; set; }
     public string? Store { get; set; }
    
