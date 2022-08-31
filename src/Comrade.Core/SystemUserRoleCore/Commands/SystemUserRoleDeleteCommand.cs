c5178022acb00ab08d67e60056e503d1af0f4519
edsonrdl <edson@EDSONRDL>
2022-08-31 17:46:27 -0300

    feat: Criado  SystemUserRoleCore

---
+++ src/Comrade.Core/SystemUserRoleCore/Commands/SystemUserRoleDeleteCommand.cs
@@ -0,0 +1,9 @@
+﻿using Comrade.Core.Bases.Interfaces;
+using Comrade.Domain.Bases;
+using Comrade.Domain.Models;
+using MediatR;
+namespace Comrade.Core.SystemUserRoleCore.Commands;
+
+public class SystemUserRoleDeleteCommand : SystemUserRole, IRequest<ISingleResult<Entity>>
+{
+}
