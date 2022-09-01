73f93e1566415c181091881c85b92159e94ede8a
edsonrdl <edson@EDSONRDL>
2022-09-01 09:12:37 -0300

    feat: Criado SystemUserRole\ Handlers

---
+++ src/Comrade.Application/Services/SystemUserRoleServices/Handlers/SystemUserRoleCreateServiceHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Comrade.Application.Bases;
+using Comrade.Application.Services.SystemUserRoleServices.Dtos;
+using Comrade.Core.SystemUserRoleCore;
+using Comrade.Core.SystemUserRoleCore.Commands;
+using MediatR;
+
+namespace Comrade.Application.Services.SystemUserRoleServices.Handlers;
+
+public class
+    SystemUserRoleCreateServiceHandler : IRequestHandler<SystemUserRoleCreateDto, SingleResultDto<EntityDto>>
+{
+    private readonly IUcSystemUserRoleCreate _createSystemUserRole;
+    private readonly IMapper _mapper;
+
+    public SystemUserRoleCreateServiceHandler(IMapper mapper, IUcSystemUserRoleCreate createSystemUserRole)
+    {
+        _mapper = mapper;
+        _createSystemUserRole = createSystemUserRole;
+    }
+
+    public async Task<SingleResultDto<EntityDto>> Handle(SystemUserRoleCreateDto request,
+        CancellationToken cancellationToken)
+    {
+        var mappedObject = _mapper.Map<SystemUserRoleCreateCommand>(request);
+        var result = await _createSystemUserRole.Execute(mappedObject).ConfigureAwait(false);
+        return new SingleResultDto<EntityDto>(result);
+    }
+}
