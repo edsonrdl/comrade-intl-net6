fcd148a748a1f555ad8b2904fe7c548665fcdf5f
edsonrdl <edson@EDSONRDL>
2022-09-01 09:39:18 -0300

    feat: Criado  SystemUserRole.cs

---
+++ src/Comrade.Domain/Models/SystemUserRole.cs
@@ -0,0 +1,28 @@
+﻿using Comrade.Domain.Bases;
+
+namespace Comrade.Domain.Models;
+
+[Table("syus_system_user_syro_role")]
+public class SystemUserRole : Entity
+{
+    public SystemUserRole()
+    {
+
+    }
+    
+    public SystemUserRole(Guid systemUserId,Guid roleId)
+    {
+        SystemUserId = systemUserId;
+        RoleId = roleId;
+        
+    }
+
+    [Column("syus_uuid_system_user")]
+    [Required(ErrorMessage = "system User Id is required")]
+    public Guid SystemUserId { get; set; }
+
+    [Column("syro_uuid_role")]
+    [Required(ErrorMessage = "Role  Id is required")]
+    public Guid RoleId { get; set; }
+
+}
