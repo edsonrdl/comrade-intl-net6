875ad9775a06d6b6c6bc1b4295f8f92617680c94
edsonrdl <edson@EDSONRDL>
2022-09-01 09:11:33 -0300

    feat: Criado SystemUserRole Dto

---
+++ src/Comrade.Application/Services/SystemUserRoleServices/Dtos/SystemUserRoleDeleteDto.cs
@@ -0,0 +1,9 @@
+﻿using Comrade.Application.Bases;
+using Comrade.Application.Services.RoleServices.Dtos;
+using MediatR;
+
+namespace Comrade.Application.Services.SystemUserRoleServices.Dtos;
+
+public class SystemUserRoleDeleteDto : SystemUserRoleDto, IRequest<SingleResultDto<EntityDto>>
+{
+}
---
+++ src/Comrade.Application/Services/SystemUserRoleServices/Dtos/SystemUserRoleDto.cs
@@ -0,0 +1,12 @@
+﻿using Comrade.Application.Bases;
+
+namespace Comrade.Application.Services.SystemUserRoleServices.Dtos;
+
+public class SystemUserRoleDto : EntityDto
+{
+    
+    public Guid SystemUserId { get; set; }
+
+    public Guid RoleId { get; set; }
+   
+}
