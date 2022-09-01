125a7beca5b509d71e62bcbce93c2655918df6ec
edsonrdl <edson@EDSONRDL>
2022-09-01 09:46:40 -0300

    feat: Criado  SystemUserRoleServices.Queries

---
+++ src/Comrade.Application/Services/SystemUserRoleServices/Queries/ISystemUserRoleQuery.cs
@@ -0,0 +1,11 @@
+﻿using Comrade.Application.Bases.Interfaces;
+using Comrade.Application.Paginations;
+using Comrade.Application.Services.SystemUserRoleServices.Dtos;
+
+namespace Comrade.Application.Services.SystemUserRoleServices.Queries;
+public interface ISystemUserRoleQuery
+{
+    Task<IPageResultDto<SystemUserRoleDto>> GetAll(PaginationQuery? paginationQuery = null);
+    Task<ISingleResultDto<SystemUserRoleDto>> GetByIdDefault(Guid id);
+    Task<ISingleResultDto<SystemUserRoleDto>> GetByIdMongo(Guid id);
+}
---
+++ src/Comrade.Application/Services/SystemUserRoleServices/Queries/SystemUserRoleQuery.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Comrade.Application.Bases;
+using Comrade.Application.Bases.Interfaces;
+using Comrade.Application.Paginations;
+using Comrade.Application.Services.SystemUserRoleServices.Dtos;
+using Comrade.Core.SystemUserRoleCore;
+using Comrade.Domain.Models;
+
+namespace Comrade.Application.Services.SystemUserRoleServices.Queries;
+
+public class SystemUserRoleQuery : ISystemUserRoleQuery
+{
+    private readonly IMapper _mapper;
+    private readonly IMongoDbQueryContext _mongoDbQueryContext;
+    private readonly ISystemUserRoleRepository _repository;
+
+    public SystemUserRoleQuery(ISystemUserRoleRepository repository,
+        IMongoDbQueryContext mongoDbQueryContext, IMapper mapper)
+    {
+        _repository = repository;
+        _mongoDbQueryContext = mongoDbQueryContext;
+        _mapper = mapper;
+    }
+
+    public async Task<IPageResultDto<SystemUserRoleDto>> GetAll(PaginationQuery? paginationQuery = null)
+    {
+        var paginationFilter = _mapper.Map<PaginationQuery?, PaginationFilter?>(paginationQuery);
+        List<SystemUserRoleDto> list;
+        if (paginationFilter == null)
+        {
+            list = await Task.Run(() => _repository.GetAllAsNoTracking()
+                .ProjectTo<SystemUserRoleDto>(_mapper.ConfigurationProvider)
+                .ToList()).ConfigureAwait(false);
+            return new PageResultDto<SystemUserRoleDto>(list);
+        }
+        var skip = (paginationFilter.PageNumber - 1) * paginationFilter.PageSize;
+        list = await Task.Run(() => _repository.GetAllAsNoTracking().Skip(skip)
+            .Take(paginationFilter.PageSize)
+            .ProjectTo<SystemUserRoleDto>(_mapper.ConfigurationProvider)
+            .ToList()).ConfigureAwait(false);
+        return new PageResultDto<SystemUserRoleDto>(paginationFilter, list);
+    }
+
+    public async Task<ISingleResultDto<SystemUserRoleDto>> GetByIdDefault(Guid id)
+    {
+        var entity = await _repository.GetById(id).ConfigureAwait(false);
+        var dto = _mapper.Map<SystemUserRoleDto>(entity);
+        return new SingleResultDto<SystemUserRoleDto>(dto);
+    }
+
+    public async Task<ISingleResultDto<SystemUserRoleDto>> GetByIdMongo(Guid id)
+    {
+        var entity = await _mongoDbQueryContext.GetById<SystemUserRole?>(id).ConfigureAwait(false);
+        var dto = _mapper.Map<SystemUserRoleDto>(entity);
+        return new SingleResultDto<SystemUserRoleDto>(dto);
+    }
+}
