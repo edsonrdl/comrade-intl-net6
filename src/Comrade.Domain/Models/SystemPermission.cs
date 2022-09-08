a1516144af011b444d439bbbb281e3153c07df9c
edsonrdl <edson@EDSONRDL>
2022-09-08 12:21:53 -0300

    feat: Modificado SystemUserRole.cs para SystemPermission.cs

---
+++ src/Comrade.Domain/Models/SystemPermission.cs
@@ -0,0 +1,28 @@
+﻿using Comrade.Domain.Bases;
+namespace Comrade.Domain.Models;
+
+[Table("sype_system_permission")]
+public class SystemPermission : Entity
+{
+    public SystemPermission()
+    {
+        Name = "";
+        Tag = "";
+    }
+    
+    public SystemPermission(string name,string tag)
+    {
+        Name = name;
+        Tag = tag;
+
+    }
+
+    [Column("sype_tx_system_permission", TypeName = "varchar")]
+    [MaxLength(255)]
+    public string Name { get; set; } = null !;
+
+    [Column("sype_tx_system_permission", TypeName = "varchar")]
+    [MaxLength(255)]
+    public string Tag { get; set; } = null!;
+
+}
